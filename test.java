6412e17c44c04c6648ff55c9540a557f40331a2c
anzhiruodu <358270236@qq.com>
2018-01-04 16:00:52 +0800

    是的

--- test.java
+++ test.java
@@ -2,6 +2,12 @@ package com.cootf.resim.charge.data.configuration;
 
 public class test {
     public void shou(){
+        System.out.check多少岁Error();
+    }
+}
+
+public class test {
+    public void shou(){
         System.out.checkError();
     }
 }
