33395e65791690d58201832f90ca0f0f09db1b48
anzhiruodu <358270236@qq.com>
2018-01-04 15:16:40 +0800

    个

--- test.java
+++ test.java
@@ -2,6 +2,6 @@ package com.cootf.resim.charge.data.configuration;
 
 public class test {
     public void shou(){
-        System.out.checkError();
+        System.out.checkError(pppppppppppppp);
     }
 }
