c654667f6b88d9b1343ddcd56538367c370e6e15
anzhiruodu <33251300+anzhiruodu@users.noreply.github.com>
2018-01-05 10:24:17 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,11 @@
 package com.cootf.resim.charge.data.configuration;
 
     public void ppp(){
+                System.out.error();
+
         System.out.error();
+                System.out.error();
+
     }
 }
 }
